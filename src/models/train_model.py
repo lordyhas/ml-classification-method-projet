3a323f756123546513cabf85dfe6b0c7ebbd7c28
lordyhas <dev.haspro@gmail.com>
2024-04-02 20:39:44 -0400

    update cross validate class

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,15 +5,30 @@ class CrossValidate(object):
 
     def __init__(self, params: dict, model, k_fold: int, metric="accuracy"):
         self.params = params
+        self.metric = metric
         self.grid_search = GridSearchCV(
             estimator=model, param_grid=self.params,
-            cv=k_fold, scoring=metric)
+            cv=k_fold, scoring=self.metric)
+
+        self.is_trained = False
+
+    def set_metric(self, metric):
+        self.metric = metric
     def train(self, x_train, y_train):
-        return self.grid_search.fit(x_train, y_train)
+        # Effectuer la recherche des hyperparamètres
+        self.grid_search.fit(x_train, y_train)
+        self.is_trained = True
+
     @property
     def best_params(self):
-        return self.grid_search.best_params_
+        # Afficher les meilleurs hyperparamètres trouvés
+
+        return self.grid_search.best_params_ \
+            if self.is_trained \
+            else {}
     @property
     def best_model(self):
-        return self.grid_search.best_estimator_
+        return self.grid_search.best_estimator_ \
+            if self.is_trained \
+            else None
 
