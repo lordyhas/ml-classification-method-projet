373161249790b3a7ce08c0519d85c2c8f9615391
lordyhas <dev.haspro@gmail.com>
2024-04-05 02:42:45 -0400

    LeafDataPreprocessing commented and update

--- src/data/preprocessing_data.py
+++ src/data/preprocessing_data.py
@@ -3,7 +3,6 @@ import numpy as np # linear algebra
 import pandas as pd
 from sklearn import model_selection
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-
 class DownloadData(object):
     def __init__(self, path):
         self.__path = path
@@ -12,14 +11,40 @@ class DownloadData(object):
         return
 
 class LeafDataPreprocessing(object):
+    """
+        Cette classe est conçue pour prétraiter les données de feuilles
+
+        Attributs:
+            - __is_normalized (bool): Indique si les données ont été normalisées.
+            - __data (pd.DataFrame): Les données brutes passées à l'objet lors de l'initialisation.
+            - __processed_data (pd.DataFrame): Une copie des données brutes qui sera traitée.
+
+        Méthodes:
+            - is_normalized(self): Retourne la valeur de l'attribut __is_normalized.
+            - __set_normalized_and_encode(self, normalized): Normalise les données et encode les cibles si 'normalized' est True.
+            - get_encode_target(self) -> pd.DataFrame: Retourne les cibles encodées si les données ont été prétraitées et normalisées.
+            - get_target(self) -> pd.DataFrame: Retourne les cibles brutes.
+            - get_unique_label(self, encoded=False): Retourne les étiquettes uniques, encodées ou non.
+            - get_classes(self) -> pd.DataFrame: Alias pour get_unique_label().
+            - x_train(self) -> pd.DataFrame: Retourne les caractéristiques (features) après prétraitement et normalisation.
+            - y_train(self, encode=True) -> pd.DataFrame: Retourne les cibles encodées ou non, selon le paramètre 'encode'.
+            - split_train_and_test(self, x_train=None, y_train=None, ratio=0.2) -> tuple: Divise les données en ensembles d'entraînement et de test.
+    """
     def __init__(self, data: pd.DataFrame, normalized=True):
+        """
+        Initialise l'objet avec les données fournies et effectue le prétraitement et la normalisation si demandé.
+
+        Paramètres:
+            - data (pd.DataFrame): Les données à prétraiter.
+            - normalized (bool): Si True, normalise les données et encode les cibles.
+        """
         self.__is_normalized = False
         self.__data = data
-        #self._test = test
-        self._processed_data = self.__data.copy()
-        #self._processed_test_data = self.__data.copy()
+        self.__processed_data = self.__data.copy()
         self.__set_normalized_and_encode(normalized=normalized)
 
+    def describe(self):
+        return self.__data.describe()
     def is_normalized(self):
         return self.__is_normalized
     def __set_normalized_and_encode(self, normalized):
@@ -29,28 +54,25 @@ class LeafDataPreprocessing(object):
 
         scaler = MinMaxScaler()
 
-
         if normalized:
             le = LabelEncoder()
             # Encoder la colonne 'species'
-            self._processed_data['species'] = le.fit_transform(self._processed_data['species'])
+            self.__processed_data['species'] = le.fit_transform(self.__processed_data['species'])
             self.__is_normalized = True
 
-        # # Mettre à l'échelle les colonnes numériques. Exclure «id» et «espèces»
+        # # Mettre à l'échelle les colonnes numériques. Exclure « id » et « espèces »
         # de la mise à l'échelle
-        numeric_cols = self._processed_data.columns.drop(['id', 'species'])
-        self._processed_data[numeric_cols] = scaler.fit_transform(self._processed_data[numeric_cols])
+        numeric_cols = self.__processed_data.columns.drop(['id', 'species'])
+        self.__processed_data[numeric_cols] = scaler.fit_transform(self.__processed_data[numeric_cols])
         self.get_classes()
 
-    def get_classes(self) -> pd.DataFrame:
-        return self._processed_data['species'].unique()
     def get_encode_target(self) -> pd.DataFrame:
         """
         La methode retourne les cibles encodées
         - si pre-traité et normalisé
         - sinon une liste vide [DataFrame([])]
         """
-        return self._processed_data['species'] if self.__is_normalized else pd.DataFrame([])
+        return self.__processed_data['species'] if self.__is_normalized else pd.DataFrame([])
     def get_target(self) -> pd.DataFrame:
         """
         La methode retourne les cibles
@@ -58,23 +80,24 @@ class LeafDataPreprocessing(object):
         return self.__data['species']
     def get_unique_label(self, encoded=False):
         return self.get_target().unique() if not encoded else self.get_encode_target().unique()
+
+    def get_classes(self) -> pd.DataFrame:
+        return self.get_unique_label()
     @property
     def x_train(self) -> pd.DataFrame:
         """
         Retourne :
         - les features normalisées et pre-traitées
-        - les cibles non pre-traitées
-        - retourne : les features [normalisée]
         """
-        return self._processed_data.drop(columns=['id', 'species'])
+        return self.__processed_data.drop(columns=['id', 'species'])
     @property
     def y_train(self, encode=True) -> pd.DataFrame:
         """
         Si encode à True cela va encoder les cibles avec un unique identifiant si déjà pre-traitées
 
         Retourne :
-        - les cibles (ou labels, classes) normalisées et pre-traitées
-        - les cibles non pre-traitées
+            - les cibles (ou labels, classes) normalisées et pre-traitées
+            - les cibles non pre-traitées
         """
         if encode:
             return self.get_encode_target()
@@ -86,11 +109,18 @@ class LeafDataPreprocessing(object):
         prend en paramètre une x_train, y_train si x_train=None ou y_train=None
         ce sont les données passées en argument de l'objet [LeafDataPreprocessing] qui seront separées
 
+        Paramètres :
+            - x_train : caractéristiques
+            - y_train : cibles
+            - ratio : les pourcentages de données test à générer
         Retourne :
-        - tuple(x_train, x_test, y_train, y_test)
+            - tuple(x_train, x_test, y_train, y_test)
         """
         if x_train is None or y_train is None:
             x_train = self.x_train
             y_train = self.y_train
         x_train, x_test, y_train, y_test = model_selection.train_test_split(x_train, y_train, test_size=ratio, random_state=42) # Test : 20%
         return x_train, x_test, y_train, y_test
+
+    def get_processed_data(self):
+        return self.__processed_data
